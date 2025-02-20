f5633ee0eda23a8808f4128500d02ced6848eb0f
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-02-20 11:03:20 +0530

    allow origin allowed

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     // origin: "http://localhost:5173",
-//     origin: "https://realtimechatty.netlify.app",
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    origin: "https://realtimechatty.netlify.app",
+    credentials: true,
+  })
+);
+// app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
