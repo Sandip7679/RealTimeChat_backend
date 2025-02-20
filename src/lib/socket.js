59a04860b4fe6da56a0ce58070c5ff10c78466f9
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-02-20 13:34:37 +0530

    transports

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -11,6 +11,7 @@ const io = new Server(server, {
     origin: ["https://realtimechatty.netlify.app"],
     // origin: "*",
     credentials: true,
+    transports: ["websocket", "polling"]
   },
 });
 
