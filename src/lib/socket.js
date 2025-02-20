fe2cdefc61f2fe15fd8bdaa96a521ca13095d5b0
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-02-20 18:19:44 +0530

    removed localhost

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: ["http://localhost:5173"],
-    origin: ["https://realtimechatty.netlify.app","http://localhost:5173"],
+    origin: ["https://realtimechatty.netlify.app"],
     // origin: "*",
     credentials: true,
     transports: ["websocket", "polling"]
